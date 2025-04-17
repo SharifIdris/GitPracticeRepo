d2b54822d262ec4c5dbd051214c0282055cc8420
Sharif <sharifidris8@gmail.com>
2025-04-17 16:38:54 +0300

    Added git2.py to .gitignore

--- git2.py
+++
@@ -1,3 +0,0 @@
-print ("Hello folks")
-print ("Welcome to the complete git crash course from beginner to advanced level")
-print ("At the end of this course, you will be farmiliar wuth how to track changes in your files ")
