11005f089e053a9c7b9b0f8aa4d5b15191512750
Sharif <sharifidris8@gmail.com>
2025-04-17 16:11:57 +0300

    Deleted git.js

---
+++ git2.py
@@ -0,0 +1,3 @@
+print ("Hello folks")
+print ("Welcome to the complete git crash course from beginner to advanced level")
+print ("At the end of this course, you will be farmiliar wuth how to track changes in your files ")
